39d9d2994fce0a5dd5872448dd110d4737ee4c11
tbholstein95 <tbholstein1@gmail.com>
2022-08-17 18:45:37 -0400

    Clean up
    Clean up default code

--- VideoControls.py
+++ VideoControls.py
@@ -20,22 +20,9 @@ def playvideo():
         print("big boy")
         value = media.get_duration()
     media_player.play()
-    #value = vlc.libvlc_media_player_get_length(media_player)
     time.sleep(value/1000)
     media_player.stop()
 
-    # ...
-    # events = media_player.event_manager()
-    # events.event_attach(vlc.EventType.MediaParsedChanged, ParseReceived)
-    # ...
-    # parseReady = 0
-    # media.parse_with_options(1, 0)
-    # while parseReady == 0:
-    #  print("working on it")
-    #
-    # # Once the flag is set, your data is ready
-    # print(media.get_parsed_status())
-    # print (media.get_duration())
 
 
 
--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import VideoControls as videoController
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-#   print_hi('PyCharm')
     videoController.playvideo()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
