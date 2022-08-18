cdc28319cba98219c7ee51d7b99be20198f90925
tbholstein95 <tbholstein1@gmail.com>
2022-08-18 12:01:24 -0400

    Add playlist
    Added ability to play "show" and "commercial" from respective folders.  Plays from one to another in this prototype.  Will want to switch to using database information long term.

--- VideoControls.py
+++ VideoControls.py
@@ -1,28 +1,75 @@
 import vlc
 import time
-
-parseReady = 0
-
-def ParseReceived(event):
-    global parseReady
-    #set a flag that your data is ready
-    parseReady = 1
+import os
 
 
-def playvideo():
-    media_player = vlc.MediaPlayer()
-    media = vlc.Media("1.mp4")
-    media_player.set_media(media)
-    media.parse_with_options(1,0)
-    while True:
-        if str(media.get_parsed_status()) == 'MediaParsedStatus.done':
-            break
-        print("big boy")
-        value = media.get_duration()
-    media_player.play()
-    time.sleep(value/1000)
-    media_player.stop()
+class VideoController:
+    def __init__(self):
+
+        self.shows = []
+        self.commercials = []
+        self.showsDirectory = "C:/Users/tbhol/codingstuff/1995NostalgiaVideoPlayer/1995NostalgiaVideoPlayer/shows"
+        self.commercialsDirectory = "C:/Users/tbhol/codingstuff/1995NostalgiaVideoPlayer/1995NostalgiaVideoPlayer/commercials"
+
+    def add_shows(self):
+        self.shows = os.listdir(self.showsDirectory)
+        print(self.shows)
+
+    def add_commercials(self):
+        self.commercials = os.listdir(self.commercialsDirectory)
+        print(self.commercials)
+
+    parseReady = 0
+
+    def ParseReceived(event):
+        global parseReady
+        #set a flag that your data is ready
+        parseReady = 1
+
+
+    # def playvideo(self):
+    #     media_player = vlc.MediaPlayer()
+    #     media = vlc.Media(self.showsDirectory + "/" + self.shows[0])
+    #     media_player.set_media(media)
+    #     media.parse_with_options(1, 0)
+    #     while True:
+    #         if str(media.get_parsed_status()) == 'MediaParsedStatus.done':
+    #             break
+    #         value = media.get_duration()
+    #
+    #     print(value)
+    #     media_player.play()
+    #     time.sleep(value/1000)
+    #     media_player.stop()
+
+    def playvideo(self):
+        media_player = vlc.MediaListPlayer()
+        player = vlc.Instance()
+        media_list = player.media_list_new()
+        media = player.media_new(self.showsDirectory + "/" + self.shows[0])
+        media_list.add_media(media)
+        media_player.set_media_list(media_list)
+
+        media.parse_with_options(1,0)
+        while True:
+            if str(media.get_parsed_status()) == 'MediaParsedStatus.done':
+                break
+            value1 = media.get_duration()
+
+        media = player.media_new(self.commercialsDirectory + "/" + self.commercials[0])
+        media_list.add_media(media)
+        media_player.set_media_list(media_list)
+
+        media.parse_with_options(1,0)
+        while True:
+            if str(media.get_parsed_status()) == 'MediaParsedStatus.done':
+                break
+            value2 = media.get_duration()
 
 
 
+        media_player.play_item_at_index(0)
+        time.sleep(value1)
+        media_player.next()
+        time.sleep(value2)
 
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import VideoControls as videoController
 
 
 if __name__ == '__main__':
-    videoController.playvideo()
+    videoC = videoController.VideoController()
+    videoC.add_shows()
+    videoC.add_commercials()
+    videoC.playvideo()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
