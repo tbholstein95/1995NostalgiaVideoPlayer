56c00e349c3e27a93c0daa9e6cb4b0672c661b0e
tbholstein95 <tbholstein1@gmail.com>
2022-08-21 20:09:02 -0400

    Finish Prototyping hardcoded video playlist
    Finished a rough prototype of the player.  Need to separate out some logic, build TKinter front end with buttons before moving on more than likely to get things connected on a basic level.

--- VideoControls.py
+++ VideoControls.py
@@ -1,6 +1,8 @@
 import vlc
 import time
 import os
+import sys
+from tkinter import *
 
 
 class VideoController:
@@ -8,8 +10,8 @@ class VideoController:
 
         self.shows = []
         self.commercials = []
-        self.showsDirectory = "Path Goes Here"
-        self.commercialsDirectory = "Path goes here"
+        self.showsDirectory = ""    #Put shows here. This will be replaced with SQL query.
+        self.commercialsDirectory = "" #Put commercials here.This will be replaced with SQL query.
 
     def add_shows(self):
         self.shows = os.listdir(self.showsDirectory)
@@ -25,6 +27,15 @@ class VideoController:
         global parseReady
         #set a flag that your data is ready
         parseReady = 1
+
+    def run(self):
+        self.mw = Tk()
+        self.mw.title('Howdy')
+        self.mw.geometry('1280x960')
+
+        self.display=Frame(self.mw, bd=5)
+        self.display.place(relwidth=1, relheight=1)
+
 
 
     # def playvideo(self):
@@ -43,11 +54,23 @@ class VideoController:
     #     media_player.stop()
 
     def playvideo(self):
-        media_player = vlc.MediaListPlayer()
+
         player = vlc.Instance()
+
+        media_player = player.media_list_player_new()
+
+        holder = vlc.libvlc_media_list_player_get_media_player(media_player)
+
+        #holder.toggle_fullscreen()
+
+
         media_list = player.media_list_new()
         media = player.media_new(self.showsDirectory + "/" + self.shows[0])
+        media.add_option('start-time=150.0')
+        media.add_option('run-time=15.0')
         media_list.add_media(media)
+
+        holder.set_hwnd(self.display.winfo_id())
         media_player.set_media_list(media_list)
 
         media.parse_with_options(1,0)
@@ -57,6 +80,8 @@ class VideoController:
             value1 = media.get_duration()
 
         media = player.media_new(self.commercialsDirectory + "/" + self.commercials[0])
+        media.add_option('start-time=0.0')
+        media.add_option('run-time=15.0')
         media_list.add_media(media)
         media_player.set_media_list(media_list)
 
@@ -67,9 +92,32 @@ class VideoController:
             value2 = media.get_duration()
 
 
+        media = player.media_new(self.showsDirectory + "/" + self.shows[0])
+        media.add_option('start-time=170.0')
+        media.add_option('run-time=15.0')
+        media_list.add_media(media)
+        media_player.set_media_list(media_list)
 
-        media_player.play_item_at_index(0)
-        time.sleep(value1)
-        media_player.next()
-        time.sleep(value2)
+        media.parse_with_options(1,0)
+        while True:
+            if str(media.get_parsed_status()) == 'MediaParsedStatus.done':
+                break
+
+        for i in range(len(media_list)):
+            print(len(media_list))
+            media_player.play_item_at_index(i)
+            if(i==0):
+                self.mw.mainloop()
+
+            time.sleep(1)
+            while media_player.is_playing():
+                pass
+            print("video")
+            if i == len(media_list)-1:
+                print("howdy")
+                break
+            else:
+                media_player.next()
+
+
 
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     videoC = videoController.VideoController()
     videoC.add_shows()
     videoC.add_commercials()
+    videoC.run()
     videoC.playvideo()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
